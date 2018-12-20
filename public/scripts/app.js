9d5a2362190bf6b5bba8ab97d9efddbf11fd035d
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-20 22:35:11 +0000

    start game renders example game url

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
 
   $("#game").click(function(){
 
-    $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
+    $.getJSON("http://localhost:3000/game/example/travel/153095", function(result){
 
       var obj = result;
       Object.keys(obj).forEach(function(key, index) {
-      console.log(key, obj[key], index);
-
+      // console.log(key, obj[key], index);
 
         if (index == 3) {
-              $("#resultJson").text("The starting URL is: " + obj.url +  "The pageid is " + obj.pageid)
+           $("#resultJson").text("This is the starting point:")
         }
+        $('#wiki-content').attr('src', obj.url);
 });
   });
 });
@@ -21,16 +21,16 @@ $(document).ready(function(){
     var integerCount = 0;
 
   $("#btnSun").click(function(){
-    $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
+    $.getJSON("http://localhost:3000/game/example/travel/153095", function(result){
       var obj = result;
 
-      Object.keys(obj).forEach(function(key, index) {
-    $("#clueResult").text("Clue: " + obj.clues[integerCount])
+Object.keys(obj).forEach(function(key, index) {
+  $("#clueResult").text("Clue: " + obj.clues[integerCount])
     integerCount ++;
     console.log(integerCount)
 
-   if (integerCount > 4){
-      integerCount = 0;
+    if (integerCount > 4){
+       integerCount = 0;
     }
 });
   });
@@ -42,13 +42,14 @@ $(document).ready(function(){
     var integerCount = 0;
 
   $("#btnSun2").click(function(){
-    $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
+    $.getJSON("http://localhost:3000/game/example/travel/153095", function(result){
       var obj = result;
 
       Object.keys(obj).forEach(function(key, index) {
-    $("#destResult").text("Destination: " + obj.destinations[integerCount])
+    $("#destResult").text("Destinations: " + obj.destinations[integerCount])
     integerCount ++;
     console.log(integerCount)
+    // $('#wiki-content').attr('src', "https://en.m.wikipedia.org/wiki/?curid=" + obj.url);
 
    if (integerCount > 4){
       integerCount = 0;
