30e1f662741a137c8b31c5f1e2210e7da9ded502
Adam Lovatt <jalovatt@hotmail.com>
2018-12-20 12:56:53 -0700

    Tidy modules

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,7 +1,7 @@
 module.exports = function(helpers) {
-  const index = require('./index')();
-  const user = require('./user')(helpers.user);
-  const game = require('./game')(helpers.game);
+  const index = require("./index")();
+  const user = require("./user")(helpers.user);
+  const game = require("./game")(helpers.game);
 
   return {index, user, game};
 };
