6720b9bfcbb2ea816a3bfa28cb451b234774ef59
Adam Lovatt <jalovatt@hotmail.com>
2018-12-20 09:26:33 -0700

    Move db/wrapper logic into db/db

--- server/db/db.js
+++ server/db/db.js
@@ -1,17 +1,64 @@
 module.exports = async function(mongo) {
 
-  const dbWrapper = require("./wrapper");
-
   const client = new mongo.MongoClient(
     process.env.MONGODB_URI,
     {useNewUrlParser: true}
   );
 
   const db = await client.connect()
-    .then((client) => {
-      return client.db(process.env.DB_NAME);
-    });
+    .then((client) => client.db(process.env.DB_NAME));
 
-  return dbWrapper(db);
+  const users = db.collection("users");
+  const games = db.collection("games");
+
+  return {
+
+    async loadGame(gameId) {
+      return await games.findOne({["_id"]: gameId});
+    },
+
+    async storeGame(game) {
+      return await games.insertOne(game);
+    },
+
+    async loadStep(gameId, step) {
+      const result = await games
+        .findOne({["_id"]: gameId}, {projection: {steps: 1}});
+
+
+      return (result && result.steps) ? result.steps[step] : null;
+    },
+
+    async findStepByArticle(gameId, articleId) {
+      const game = await this.loadGame(gameId);
+      const step = game.steps.find((step) => Number(step.pageid) === Number(articleId));
+
+      return (step) ? step : null;
+    },
+
+    async findUserByEmail(email) {
+      const searchEmail = email.trim().toLowerCase();
+
+      return await users.findOne({["_id"]: email});
+    },
+
+    async isEmailAvailable(email) {
+      const searchEmail = email.trim().toLowerCase();
+      const result = await users.findOne({["_id"]: searchEmail});
+
+      return !result;
+    },
+
+    async isNameAvailable(name) {
+      const result = await users.findOne({name});
+
+      return !result;
+    },
+
+    async registerUser(user) {
+      await users.insertOne(user);
+    }
+
+  };
 
 };
