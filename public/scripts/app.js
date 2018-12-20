e9639f13d5bb84fb07b5cdf4acd0ea950f37a1f7
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-20 21:31:31 +0000

    rednering destinations

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
   $("#game").click(function(){
 
     $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
@@ -10,22 +9,9 @@ $(document).ready(function(){
       console.log(key, obj[key], index);
 
 
-
         if (index == 3) {
-
-              $("#resultJson").text("The starting URL is: " + obj.url)
-
+              $("#resultJson").text("The starting URL is: " + obj.url +  "The pageid is " + obj.pageid)
         }
-
-
-      // if ("div".append === "The starting URL is: " + obj.url) {
-      //   console.log("apple")
-      // }
-      //  if (index == 1) {
-      //   console.log(obj.pageid)
-      // }
-      // console.log(obj.gameid.index)
-      // console.log(obj.gameid[index])
 });
   });
 });
@@ -34,24 +20,39 @@ $(document).ready(function(){
 $(document).ready(function(){
     var integerCount = 0;
 
-
   $("#btnSun").click(function(){
-
     $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
-
       var obj = result;
+
       Object.keys(obj).forEach(function(key, index) {
-
-
     $("#clueResult").text("Clue: " + obj.clues[integerCount])
     integerCount ++;
     console.log(integerCount)
 
-    if (integerCount > 4){
+   if (integerCount > 4){
       integerCount = 0;
     }
+});
+  });
+});
+});
 
 
+$(document).ready(function(){
+    var integerCount = 0;
+
+  $("#btnSun2").click(function(){
+    $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
+      var obj = result;
+
+      Object.keys(obj).forEach(function(key, index) {
+    $("#destResult").text("Destination: " + obj.destinations[integerCount])
+    integerCount ++;
+    console.log(integerCount)
+
+   if (integerCount > 4){
+      integerCount = 0;
+    }
 });
   });
 });
