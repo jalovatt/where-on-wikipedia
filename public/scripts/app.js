2d488d305e1c5c4ad1679ed9ae496e021ddc493c
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-20 18:18:17 +0000

    initial scripts for appending clues/ example game data into div

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,33 +5,74 @@ $(document).ready(function(){
 
     $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
 
-      // $.each(result, function(i, field){
-
-      //   $("div").append(field + " ");
-      //   console.log(JSON.gameid)
-      // });
-      for (let results in result){
-      if (result.gameid == "example") {
-      // console.log(result[r])
-      console.log(result.gameid)
-
-      console.log(result.title)
-
-      console.log("the result is: ", result.gameid);
-      // return result.gameid
-        $("div").append("The initial start page is:" + result.url);
-    }
-      else {
-        console.log('nope')
-      }}
-    });
+      var obj = result;
+      Object.keys(obj).forEach(function(key, index) {
+      // console.log(key, obj[key], index);
+      // console.log(key, index)
+      if (index == 3) {
+            $("div").append("The starting URL is: " + obj.url)
+      }
+      //  if (index == 1) {
+      //   console.log(obj.pageid)
+      // }
+      // console.log(obj.gameid.index)
+      // console.log(obj.gameid[index])
+});
   });
 });
-
-$(document).ready(function() {
-  $('#btnSun').click(myFunction);
 });
 
-function myFunction() {
-  alert('hi');
-}
+$(document).ready(function(){
+
+
+  $("#btnSun").click(function(){
+
+    $.getJSON("http://localhost:3000/game/0/travel/153095", function(result){
+
+      var obj = result;
+      Object.keys(obj).forEach(function(key, index) {
+      // console.log(key, obj[key], index);
+      // console.log(key, index)
+      if (index == 5) {
+            $("div").append("Clue: " + obj.clues[0])
+      }
+
+});
+  });
+});
+});
+
+// $(document).ready(function() {
+//   $('#btnSun').click(myFunction);
+// });
+
+// function myFunction() {
+//   alert('hi');
+// }
+
+
+
+      // $.each(result, function(i, field){
+
+//       //   $("div").append(field + " ");
+//       //   console.log(JSON.gameid)
+//       // });
+// // var res = ''
+//       if (result.url == "https://en.wikipedia.org/wiki/Radio_navigation") {
+//       for (let results in result){
+//         let value = result[results];
+//       // console.log(result.gameid)
+// // res += result[results].split(":", 1)[0];
+
+//       // console.log(result.destinations[0])
+// // console.log(res)
+// // console.log(result)
+//       // console.log("the result is: ", result.clues[0]);
+//       // return result.gameid
+//       console.log(result.gameid)
+//         $("div").append("The initial start page is:" + result.url);
+//     }}
+//       else {
+//         console.log('nope')
+//       }
+//     });
