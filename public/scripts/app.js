3d54b91d840469d02718a174576adfb0f6f69de9
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-21 00:30:39 +0000

    start game button generates a new game with data from db. clues also generated as such

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,29 @@
+// $(document).ready(function(){
+
+//   $("#game").click(function(){
+
+//     $.getJSON("/game/new/", function(result){
+//      console.log(result)
+
+//   });
+// });
+// });
+
 $(document).ready(function(){
 
   $("#game").click(function(){
 
-    $.getJSON("http://localhost:3000/game/example/travel/153095", function(result){
+    $.getJSON("/game/new/", function(result){
 
       var obj = result;
+      console.log(result)
       Object.keys(obj).forEach(function(key, index) {
       // console.log(key, obj[key], index);
 
-        if (index == 3) {
+        if (index) {
            // $("#resultJson").text("This is the starting point:")
            $('#wiki-content').attr('src', obj.url);
         }
-
 });
   });
 });
@@ -22,7 +33,7 @@ $(document).ready(function(){
     var integerCount = 0;
 
   $("#btnSun").click(function(){
-    $.getJSON("http://localhost:3000/game/example/travel/153095", function(result){
+    $.getJSON("/game/new/", function(result){
       var obj = result;
 
 Object.keys(obj).forEach(function(key, index) {
@@ -39,27 +50,32 @@ Object.keys(obj).forEach(function(key, index) {
 });
 
 
-$(document).ready(function(){
-    var integerCount = 0;
+// $(document).ready(function(){
+//     var integerCount = 0;
 
-  $("#btnSun2").click(function(){
-    $.getJSON("http://localhost:3000/game/example/travel/153095", function(result){
-      var obj = result;
+//   $("#btnSun2").click(function(){
+//     $.getJSON("http://localhost:3000/game/example/travel/153095", function(result){
+//       var obj = result;
 
-      Object.keys(obj).forEach(function(key, index) {
-    $("#destResult").text(obj.destinations[integerCount])
-    $('#wiki-content').attr('src', "https://en.m.wikipedia.org/wiki/" + obj.destinations[integerCount]);
-    integerCount ++;
-    console.log(integerCount)
-    // $('#wiki-content').attr('src', "https://en.m.wikipedia.org/wiki/" + destinations[integerCount]);
+//       Object.keys(obj).forEach(function(key, index) {
+//     $("#destResult").text(obj.destinations[integerCount])
+//     $('#wiki-content').attr('src', "https://en.m.wikipedia.org/wiki/" + obj.destinations[integerCount]);
+//     integerCount ++;
+//     console.log(integerCount)
+//     // console.log(/game/new)
+//     // $('#wiki-content').attr('src', "https://en.m.wikipedia.org/wiki/" + destinations[integerCount]);
 
-   if (integerCount > 4){
-      integerCount = 0;
-    }
-});
-  });
-});
-});
+//    if (integerCount > 4){
+//       integerCount = 0;
+//     }
+// });
+//   });
+// });
+// });
+
+
+
+
 
 
 
