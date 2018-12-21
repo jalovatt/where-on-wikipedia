07b0ce088647bd394f5de6652639bb5be285c9e1
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-21 21:44:56 +0000

    added a loading animation when the start game button is pushed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,7 @@
-// $(document).ready(function(){
-
-//   $("#game").click(function(){
-
-//     $.getJSON("/game/new/", function(result){
-//      console.log(result)
-
-//   });
-// });
-// });
-
 $(document).ready(function(){
 
   $("#game").click(function(){
-
+  $("#hide").removeClass("hidden")
     $.getJSON("/game/new/", function(result){
 
       var obj = result;
@@ -20,7 +9,7 @@ $(document).ready(function(){
       Object.keys(obj).forEach(function(key, index) {
       // console.log(key, obj[key], index);
 
-        if (index) {
+        if (index == true) {
            $('#wiki-content').attr('src', obj.url); //button for starting game
            $("#resultJson").attr('href', obj.url);
            $("#resultJson").text("Starting Article: " + obj.title) //button for rendering clues
