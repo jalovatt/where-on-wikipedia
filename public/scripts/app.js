6cceffe561bb40c467381ff307aeb053611c285c
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-27 19:27:00 +0000

    removed redundant code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,13 +9,11 @@ $(document).ready(function(){
       Object.keys(obj).forEach(function(key, index) {
       // console.log(key, obj[key], index);
 
-        if (index == true) {
            $('#wiki-content').attr('src', obj.url); //button for starting game
            $("#resultJson").attr('href', obj.url);
            $("#resultJson").text("Starting Article: " + obj.title) //button for rendering clues
            $("#clueResult").empty();
            $("#destResult").empty();
-        }
   });
 
       var integerCount = 0;
@@ -37,7 +35,7 @@ $(document).ready(function(){
         Object.keys(obj).forEach(function(key, index) {
 
       $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[integerCount2]);
-      $("#destResult").text("Article number " + (integerCount2 + 1) + ": " + obj.destinations[integerCount2])
+      $("#destResult").text("Article number " + (integerCount2 + 1) + ": " + obj.destinations[integerCount2]) //button for rendering articles
       integerCount2 ++;
 
 
