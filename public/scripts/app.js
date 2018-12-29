753edf95a31907dc54b862e03a8e4bd05ab10d97
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-29 19:35:56 +0000

    tidied up the code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,11 +23,11 @@ $(document).ready(function(){
       $("#btnSun").click(function(){
         Object.keys(obj).forEach(function(key, index) {
 
-      $("#clueResult").text("Clue number " + (integerCount + 1) + ": " + obj.clues[integerCount]) //button for rendering clues
-      integerCount ++;
+           $("#clueResult").text("Clue number " + (integerCount + 1) + ": " + obj.clues[integerCount]) //button for rendering clues
+             integerCount ++;
 
-      if (integerCount > 4){
-        integerCount = 0;
+           if (integerCount > 4){
+             integerCount = 0;
     }
 });
   });
@@ -35,17 +35,17 @@ $(document).ready(function(){
       $("#btnSun2").click(function(){
         Object.keys(obj).forEach(function(key, index) {
 
-      $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[0]); //button for rendering articles
-      $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[1]);
-      $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[2]);
-      $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[3]);
-      $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[4]);
+           $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[0]); //button for rendering articles
+           $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[1]);
+           $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[2]);
+           $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[3]);
+           $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[4]);
 
-      $("#destResult").text("Article number 1: " + obj.destinations[0])
-      $("#destResult1").text("Article number 2: " + obj.destinations[1])
-      $("#destResult2").text("Article number 3: " + obj.destinations[2])
-      $("#destResult3").text("Article number 4: " + obj.destinations[3])
-      $("#destResult4").text("Article number 5: " + obj.destinations[4])
+           $("#destResult").text("Article number 1: " + obj.destinations[0])
+           $("#destResult1").text("Article number 2: " + obj.destinations[1])
+           $("#destResult2").text("Article number 3: " + obj.destinations[2])
+           $("#destResult3").text("Article number 4: " + obj.destinations[3])
+           $("#destResult4").text("Article number 5: " + obj.destinations[4])
 
 });
   });
