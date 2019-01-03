2691a8b3aa83b37b91665c1333499a15484f3a27
Bryan Kenny <b.93.kenny@gmail.com>
2019-01-03 21:07:12 +0000

    changed some code to comport with the destination as an object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,8 +5,11 @@ $(document).ready(function(){
     $.getJSON("/game/new/", function(result){
 
       var obj = result;
-      // console.log(obj)
-      // console.log(obj.gameid)
+      console.log(obj)
+      console.log(obj.destinations)
+      console.log(obj.destinations[0])
+      console.log(obj.destinations[0].id)
+      console.log(obj.destinations[0].title)
       Object.keys(obj).forEach(function(key, index) {
 
            $('#wiki-content').attr('src', obj.url); //button for starting game
@@ -37,17 +40,17 @@ $(document).ready(function(){
       $("#btnSun2").click(function(){
         Object.keys(obj).forEach(function(key, index) {
 
-           $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[0]); //button for rendering articles
-           $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[1]);
-           $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[2]);
-           $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[3]);
-           $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[4]);
+           $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[0].title); //button for rendering articles
+           $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[1].title);
+           $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[2].title);
+           $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[3].title);
+           $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + obj.destinations[4].title);
 
-           $("#destResult").text("Article number 1: " + obj.destinations[0])
-           $("#destResult1").text("Article number 2: " + obj.destinations[1])
-           $("#destResult2").text("Article number 3: " + obj.destinations[2])
-           $("#destResult3").text("Article number 4: " + obj.destinations[3])
-           $("#destResult4").text("Article number 5: " + obj.destinations[4])
+           $("#destResult").text("Article number 1: " + obj.destinations[0].title)
+           $("#destResult1").text("Article number 2: " + obj.destinations[1].title)
+           $("#destResult2").text("Article number 3: " + obj.destinations[2].title)
+           $("#destResult3").text("Article number 4: " + obj.destinations[3].title)
+           $("#destResult4").text("Article number 5: " + obj.destinations[4].title)
 
      //  $("#destResult").click(function(){
      //     console.log("destination link clicked on and iFrame changed")
