2f5e5c2062d67b5700a59149b6ead15ebc3ea6bc
Bryan Kenny <b.93.kenny@gmail.com>
2019-01-06 05:18:18 +0000

    if the correct article is clicked on the new list or articles is rendered

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,13 +22,24 @@ $(document).ready(function(){
 
       $("#btnSun").click(function(){
         Object.keys(obj).forEach(function(key, index) {
+          click = 1;
+          $("#clueResult").removeClass("hidden")
+          if (click = 1) {
+            $("#clueResult1").removeClass("hidden")
+          }
 
            $("#clueResult").text("Clue number " + (integerCount + 1) + ": " + obj.clues[integerCount]) //button for rendering clues
              integerCount ++;
 
            if (integerCount > 4){
              integerCount = 0;
-    }
+           // $("#clueResult").text("clue number 1: " + obj.clues[0])
+           // $("#clueResult1").text("clue number 2: " + obj.clues[1])
+           // $("#clueResult2").text("clue number 3: " + obj.clues[2])
+           // $("#clueResult3").text("clue number 4: " + obj.clues[3])
+           // $("#clueResult4").text("clue number 5: " + obj.clues[4])
+        }
+
 });
   });
 
@@ -58,12 +69,32 @@ $(document).ready(function(){
           let object = res;
 
         if (object.deadend == true){
-          $("#finalArticle").text("The article you're looking for isn't here!")
-          console.log(object.clues)
+          // $("#finalArticle").text("The article you're looking for isn't here!")
+          $("#destResult").text("Article number 1: " + object.destinations[0])
+          $("#destResult1").text("Article number 2: " + object.destinations[1])
+          $("#destResult2").text("Article number 3: " + object.destinations[2])
+          $("#destResult3").text("Article number 4: " + object.destinations[3])
+          $("#destResult4").text("Article number 5: " + object.destinations[4])
+          // console.log(object.clues)
+          console.log(object)
+          console.log(object.destinations)
         }
         else {
-         $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
-         console.log(object.clues)
+          // $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          $("#destResult").text("Article number 1: " + object.destinations[0].title)
+          $("#destResult1").text("Article number 2: " + object.destinations[1].title)
+          $("#destResult2").text("Article number 3: " + object.destinations[2].title)
+          $("#destResult3").text("Article number 4: " + object.destinations[3].title)
+          $("#destResult4").text("Article number 5: " + object.destinations[4].title)
+
+          $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[0].title); //button for rendering articles
+          $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[1].title);
+          $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[2].title);
+          $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[3].title);
+          $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[4].title);
+         // console.log(object.clues)
+         console.log(object)
+         console.log(object.destinations)
         }
           });
 
@@ -75,12 +106,31 @@ $(document).ready(function(){
           let object = res;
 
         if (object.deadend == true){
-          $("#finalArticle").text("The article you're looking for isn't here!")
+          // $("#finalArticle").text("The article you're looking for isn't here!")
+          $("#destResult").text("Article number 1: " + object.destinations[0])
+          $("#destResult1").text("Article number 2: " + object.destinations[1])
+          $("#destResult2").text("Article number 3: " + object.destinations[2])
+          $("#destResult3").text("Article number 4: " + object.destinations[3])
+          $("#destResult4").text("Article number 5: " + object.destinations[4])
           console.log(object.clues)
+          console.log(object.destinations)
         }
         else {
-         $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          // $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          $("#destResult").text("Article number 1: " + object.destinations[0].title)
+          $("#destResult1").text("Article number 2: " + object.destinations[1].title)
+          $("#destResult2").text("Article number 3: " + object.destinations[2].title)
+          $("#destResult3").text("Article number 4: " + object.destinations[3].title)
+          $("#destResult4").text("Article number 5: " + object.destinations[4].title)
+
+          $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[0].title); //button for rendering articles
+          $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[1].title);
+          $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[2].title);
+          $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[3].title);
+          $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[4].title);
+
          console.log(object.clues)
+         console.log(object.destinations)
         }
           });
 
@@ -92,12 +142,30 @@ $(document).ready(function(){
           let object = res;
 
         if (object.deadend == true){
-          $("#finalArticle").text("The article you're looking for isn't here!")
+          // $("#finalArticle").text("The article you're looking for isn't here!")
+          $("#destResult").text("Article number 1: " + object.destinations[0])
+          $("#destResult1").text("Article number 2: " + object.destinations[1])
+          $("#destResult2").text("Article number 3: " + object.destinations[2])
+          $("#destResult3").text("Article number 4: " + object.destinations[3])
+          $("#destResult4").text("Article number 5: " + object.destinations[4])
           console.log(object.clues)
+          console.log(object.destinations)
         }
         else {
-         $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          // $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          $("#destResult").text("Article number 1: " + object.destinations[0].title)
+          $("#destResult1").text("Article number 2: " + object.destinations[1].title)
+          $("#destResult2").text("Article number 3: " + object.destinations[2].title)
+          $("#destResult3").text("Article number 4: " + object.destinations[3].title)
+          $("#destResult4").text("Article number 5: " + object.destinations[4].title)
+
+          $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[0].title); //button for rendering articles
+          $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[1].title);
+          $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[2].title);
+          $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[3].title);
+          $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[4].title);
          console.log(object.clues)
+         console.log(object.destinations)
         }
           });
 
@@ -109,12 +177,30 @@ $(document).ready(function(){
           let object = res;
 
         if (object.deadend == true){
-          $("#finalArticle").text("The article you're looking for isn't here!")
+          // $("#finalArticle").text("The article you're looking for isn't here!")
+          $("#destResult").text("Article number 1: " + object.destinations[0])
+          $("#destResult1").text("Article number 2: " + object.destinations[1])
+          $("#destResult2").text("Article number 3: " + object.destinations[2])
+          $("#destResult3").text("Article number 4: " + object.destinations[3])
+          $("#destResult4").text("Article number 5: " + object.destinations[4])
           console.log(object.clues)
+          console.log(object.destinations)
         }
         else {
-         $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          // $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          $("#destResult").text("Article number 1: " + object.destinations[0].title)
+          $("#destResult1").text("Article number 2: " + object.destinations[1].title)
+          $("#destResult2").text("Article number 3: " + object.destinations[2].title)
+          $("#destResult3").text("Article number 4: " + object.destinations[3].title)
+          $("#destResult4").text("Article number 5: " + object.destinations[4].title)
+
+          $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[0].title); //button for rendering articles
+          $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[1].title);
+          $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[2].title);
+          $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[3].title);
+          $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[4].title);
          console.log(object.clues)
+         console.log(object.destinations)
         }
           });
 
@@ -126,12 +212,30 @@ $(document).ready(function(){
            let object = res;
 
         if (object.deadend == true){
-          $("#finalArticle").text("The article you're looking for isn't here!")
+          // $("#finalArticle").text("The article you're looking for isn't here!")
+          $("#destResult").text("Article number 1: " + object.destinations[0])
+          $("#destResult1").text("Article number 2: " + object.destinations[1])
+          $("#destResult2").text("Article number 3: " + object.destinations[2])
+          $("#destResult3").text("Article number 4: " + object.destinations[3])
+          $("#destResult4").text("Article number 5: " + object.destinations[4])
           console.log(object.clues)
+          console.log(object.destinations)
         }
         else {
-         $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
-         console.log(object.clues)
+          // $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
+          $("#destResult").text("Article number 1: " + object.destinations[0].title)
+          $("#destResult1").text("Article number 2: " + object.destinations[1].title)
+          $("#destResult2").text("Article number 3: " + object.destinations[2].title)
+          $("#destResult3").text("Article number 4: " + object.destinations[3].title)
+          $("#destResult4").text("Article number 5: " + object.destinations[4].title)
+
+          $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[0].title); //button for rendering articles
+          $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[1].title);
+          $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[2].title);
+          $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[3].title);
+          $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + object.destinations[4].title);
+          console.log(object.clues)
+          console.log(object.destinations)
         }
           });
 
