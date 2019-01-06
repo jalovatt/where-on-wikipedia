5221f8d8f174e16d7f623062c731394be7a842c3
Adam Lovatt <jalovatt@hotmail.com>
2019-01-06 13:48:49 -0700

    Add button to play example game, continue tidying

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,27 @@
 $(document).ready(function(){
 
-  function populateDestinations(dests) {
+  function populateDestinations(obj) {
 
-    if (dests.deadend == true){
-      // $("#finalArticle").text("The article you're looking for isn't here!")
-      $("#destResult").text("Article number 1: " + dests.destinations[0])
-      $("#destResult1").text("Article number 2: " + dests.destinations[1])
-      $("#destResult2").text("Article number 3: " + dests.destinations[2])
-      $("#destResult3").text("Article number 4: " + dests.destinations[3])
-      $("#destResult4").text("Article number 5: " + dests.destinations[4])
-      // console.log(object.clues)
-      console.log(dests)
-      console.log(dests.destinations)
+    if (obj.deadend == true){
+      $("#destResult").text(obj.destinations[0]);
+      $("#destResult").removeAttr("href");
+      $("#destResult1").empty();
+      $("#destResult2").empty();
+      $("#destResult3").empty();
+      $("#destResult4").empty();
     }
     else {
-      // $("#finalArticle").text("The suspect is hiding in: " + object.destinations[0].title)
-      $("#destResult").text("Article number 1: " + dests.destinations[0].title)
-      $("#destResult1").text("Article number 2: " + dests.destinations[1].title)
-      $("#destResult2").text("Article number 3: " + dests.destinations[2].title)
-      $("#destResult3").text("Article number 4: " + dests.destinations[3].title)
-      $("#destResult4").text("Article number 5: " + dests.destinations[4].title)
+      $("#destResult0").text("Article number 1: " + obj.destinations[0].title);
+      $("#destResult1").text("Article number 2: " + obj.destinations[1].title);
+      $("#destResult2").text("Article number 3: " + obj.destinations[2].title);
+      $("#destResult3").text("Article number 4: " + obj.destinations[3].title);
+      $("#destResult4").text("Article number 5: " + obj.destinations[4].title);
 
-      $("#destResult").attr('href', "https://en.m.wikipedia.org/wiki/" + dests.destinations[0].title); //button for rendering articles
-      $("#destResult1").attr('href', "https://en.m.wikipedia.org/wiki/" + dests.destinations[1].title);
-      $("#destResult2").attr('href', "https://en.m.wikipedia.org/wiki/" + dests.destinations[2].title);
-      $("#destResult3").attr('href', "https://en.m.wikipedia.org/wiki/" + dests.destinations[3].title);
-      $("#destResult4").attr('href', "https://en.m.wikipedia.org/wiki/" + dests.destinations[4].title);
+      $("#destResult0").attr("href", "https://en.m.wikipedia.org/wiki/" + obj.destinations[0].title); //button for rendering articles
+      $("#destResult1").attr("href", "https://en.m.wikipedia.org/wiki/" + obj.destinations[1].title);
+      $("#destResult2").attr("href", "https://en.m.wikipedia.org/wiki/" + obj.destinations[2].title);
+      $("#destResult3").attr("href", "https://en.m.wikipedia.org/wiki/" + obj.destinations[3].title);
+      $("#destResult4").attr("href", "https://en.m.wikipedia.org/wiki/" + obj.destinations[4].title);
     }
   }
 
@@ -46,67 +42,57 @@ $(document).ready(function(){
   $("#btn-menu").click(showTab);
   $("#btn-help").click(showTab);
 
+  function initializeGame(obj) {
+
+    $("#wiki-content").attr("src", obj.url); //button for starting game
+    $("#resultJson").attr("href", obj.url);
+    $("#resultJson").text("Starting Article: " + obj.title);
+    $("#clueResult").empty();
+    $("#destResult0").empty();
+    $("#destResult1").empty();
+    $("#destResult2").empty();
+    $("#destResult3").empty();
+    $("#destResult4").empty();
+    $("#finalArticle").empty();
+
+    populateDestinations(obj);
+
+    var integerCount = 0;
+
+    $("#btnSun").click(function(){
+      $("#clueResult").text("Clue number " + (integerCount + 1) + ": " + obj.clues[integerCount]); //button for rendering clues
+      integerCount ++;
+
+      if (integerCount > 4){
+        integerCount = 0;
+      }
+    });
+
+    $("#btnSun2").click(function(){
+      $("#finalArticle").empty();
+      populateDestinations(obj);
+    });
+
+    function travelTo(gameId, articleId) {
+      $.getJSON("/game/" + gameId + "/travel/" + articleId, populateDestinations);
+    }
+
+    $("#destResult0").click( function() { travelTo(obj.gameid, obj.destinations[0].id);});
+    $("#destResult1").click( function() { travelTo(obj.gameid, obj.destinations[1].id);});
+    $("#destResult2").click( function() { travelTo(obj.gameid, obj.destinations[2].id);});
+    $("#destResult3").click( function() { travelTo(obj.gameid, obj.destinations[3].id);});
+    $("#destResult4").click( function() { travelTo(obj.gameid, obj.destinations[4].id);});
+
+  }
+
   $("#game").click(function(){
     $("#hide").removeClass("hidden");
-    $.getJSON("/game/new/", function(result){
-      let obj = result;
-      Object.keys(obj).forEach(function(key, index) {
+    $.getJSON("/game/new/", initializeGame);
+  });
 
-        $('#wiki-content').attr('src', obj.url); //button for starting game
-        $("#resultJson").attr('href', obj.url);
-        $("#resultJson").text("Starting Article: " + obj.title)
-        $("#clueResult").empty();
-        $("#destResult").empty();
-        $("#destResult1").empty();
-        $("#destResult2").empty();
-        $("#destResult3").empty();
-        $("#destResult4").empty();
-        $("#finalArticle").empty();
-      });
-
-      var integerCount = 0;
-
-      $("#btnSun").click(function(){
-        Object.keys(obj).forEach(function(key, index) {
-          click = 1;
-          $("#clueResult").removeClass("hidden")
-          if (click = 1) {
-            $("#clueResult1").removeClass("hidden")
-          }
-
-          $("#clueResult").text("Clue number " + (integerCount + 1) + ": " + obj.clues[integerCount]) //button for rendering clues
-          integerCount ++;
-
-          if (integerCount > 4){
-            integerCount = 0;
-          }
-
-        });
-      });
-
-      $("#btnSun2").click(function(){
-        Object.keys(obj).forEach(function(key, index) {
-
-          $("#finalArticle").empty();
-          populateDestinations(obj);
-
-        });
-      });
-
-      function travelTo(gameId, articleId) {
-        $.getJSON("/game/" + gameId + "/travel/" + articleId, populateDestinations);
-      }
-
-      $("#destResult").click( travelTo(obj.gameid, obj.destinations[0].id) );
-
-      $("#destResult1").click( travelTo(obj.gameid, obj.destinations[1].id) );
-
-      $("#destResult2").click( travelTo(obj.gameid, obj.destinations[2].id) );
-
-      $("#destResult3").click( travelTo(obj.gameid, obj.destinations[3].id) );
-
-      $("#destResult4").click( travelTo(obj.gameid, obj.destinations[4].id) );
-    });
+  $("#game-example").click(function () {
+    $("#hide").removeClass("hidden");
+    $.getJSON("/game/example/", initializeGame);
   });
 
 });
