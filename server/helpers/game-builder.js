1d6644191c31d1a26bf32a7ecaa8ecc1ca9436b5
Bryan Kenny <b.93.kenny@gmail.com>
2019-01-09 23:57:33 +0000

    when the user naviagtes through to the final article, a more explicit message comes up telling them to confirm the suspect before proceeding with their search

--- server/helpers/game-builder.js
+++ server/helpers/game-builder.js
@@ -239,7 +239,7 @@ module.exports = function(wiki) {
 
       const last = steps.length - 1;
       steps[last].destinations = await this.addDestinations(steps[last]);
-      steps[last].clues = ["Keep your eyes peeled - the thief is nearby!"];
+      steps[last].clues = ["Keep your eyes peeled - the thief is nearby! \n Before hitting search again to confirm you have the correct suspect, navigate to the suspects tab to identify the thief from the list of perps" ];
       steps[last].finalStep = true;
 
       return steps;
